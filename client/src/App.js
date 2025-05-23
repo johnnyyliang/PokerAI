363228b47ac0cc4f6e37069a61e118fed80572bc
Jonathan Liang <jonathan.liang3@gmail.com>
2025-05-23 14:31:01 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+import PlayPage from './pages/PlayPage';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/play" element={<PlayPage />} />
       </Routes>
     </Router>
   );
