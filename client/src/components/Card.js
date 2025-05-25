c5dc83d3792d6b49b59016b75cc6bf30e7de3793
Jonathan Liang <jonathan.liang3@gmail.com>
2025-05-25 16:10:11 -0400

    card component rank and suit implemented

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -2,3 +2,17 @@ import React from 'react';
 import CardSuit from './CardSuit';
 import './Card.css';
 
+const Card = ( {rank, suit} ) => {
+    return (
+        <div className = "card-container">
+            <div className = "rank">
+                <p> {rank} </p>
+            </div>
+            <div className = "suit">
+                <CardSuit suit = {suit} color = "black" />
+            </div>
+        </div>
+    );
+};
+
+export default Card;
