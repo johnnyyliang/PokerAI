b5f7455cd4d145d07fb6e7044e413988ef8dc745
Jonathan Liang <jonathan.liang3@gmail.com>
2025-05-25 16:11:19 -0400

    basic outline for playpage complete

--- client/src/pages/PlayPage.js
+++ client/src/pages/PlayPage.js
@@ -3,6 +3,8 @@ import { Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 import './PlayPage.css';
 import { createTheme, ThemeProvider} from '@mui/material/styles';
+import Card from '../components/Card'
+import cardTurnedOver from '../assets/card-turned-over.png'
 
 const blackwhiteTheme = createTheme({
     palette: {
@@ -20,6 +22,31 @@ function PlayPage() {
                     <Button variant="contained" component={Link} to="/" className='back-button'>Back</Button>
                 </ThemeProvider>
             <h1 className='title'>Play</h1>
+            </div>
+            <h1 className = 'text'> AI Cards </h1>
+            <div className = "ai-cards">
+                <img src = { cardTurnedOver } alt = "Card Turned Over" className = "turned-over"></img>
+                <img src = { cardTurnedOver } alt = "Card Turned Over" className = "turned-over"></img>
+            </div>
+            <div className = "community-cards">
+                <img src = { cardTurnedOver } alt = "Card Turned Over" className = "turned-over"></img>
+                <img src = { cardTurnedOver } alt = "Card Turned Over" className = "turned-over"></img>
+                <img src = { cardTurnedOver } alt = "Card Turned Over" className = "turned-over"></img>
+                <img src = { cardTurnedOver } alt = "Card Turned Over" className = "turned-over"></img>
+                <img src = { cardTurnedOver } alt = "Card Turned Over" className = "turned-over"></img>
+            </div>
+            <div className = "self-cards">
+                <img src = { cardTurnedOver } alt = "Card Turned Over" className = "turned-over"></img>
+                <img src = { cardTurnedOver } alt = "Card Turned Over" className = "turned-over"></img>
+            </div>
+            <h1 className = "text"> Your Hand </h1>
+            <div className = "option-container">
+                <ThemeProvider theme = {blackwhiteTheme} className = "options">
+                    <Button variant = "contained" className = "option"> Bet </Button>
+                    <Button variant = "contained" className = "option"> Call </Button>
+                    <Button variant = "contained" className = "option"> Fold </Button>
+                    <Button variant = "contained" className = "option"> Check </Button>
+                </ThemeProvider>
             </div>
         </div>
     );
